4b4c001fd610a3ec49ab90e67fbad1d7ed3a2330
Wilmer Ocampo Quispe <Wilmer_Ocampo@hotmail.com>
2024-07-08 23:34:24 -0500

    Actualizando env.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -20,15 +20,16 @@ import {AngularFireDatabaseModule} from "@angular/fire/compat/database";
 import {AngularFireAuthModule} from "@angular/fire/compat/auth";
 import {AngularFirestoreModule} from "@angular/fire/compat/firestore";
 import {AngularFireStorageModule} from "@angular/fire/compat/storage";
+import * as process from "node:process";
 
 const firebaseConfig = {
-  apiKey: '${process.env.API_KEY}',
-  authDomain: '${process.env.AUTH_DOMAIN}',
-  databaseURL: '${process.env.DATABASE_URL}',
-  projectId: '${process.env.PROJECT_ID}',
-  storageBucket: '$process.env.STORAGE_BUCKET',
-  messagingSenderId: '$process.env.MESSAGING_SENDER_ID',
-  appId: '${process.env.APP_ID}'
+  apiKey: process.env['API_KEY'] as string,
+  authDomain: process.env['AUTH_DOMAIN'] as string,
+  databaseURL: process.env['DATABASE_URL'],
+  projectId: process.env['PROJECT_ID'] as string,
+  storageBucket: process.env['STORAGE_BUCKET'] as string,
+  messagingSenderId: process.env['MESSAGING_SENDER_ID'] as string,
+  appId: process.env['APP_ID'] as string
 };
 
 @NgModule({
